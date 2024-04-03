5bc5afeb597dfd1dbe23502ee7cafbd7a23e16a2
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-04-03 09:58:52 +0300

    Route Yapıs

---
+++ src/pages/introduce/Introduce.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Introduce.css';
+
+export const Introduce = () => {
+    return (
+        <div className='introduce'>Devrim</div>
+    )
+}
+
+export default Introduce
